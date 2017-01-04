d8ad3cf0e15a7b4b83e803195cd2e89e502403b3
Vicens Fayos <datracka@gmail.com>
2017-01-04 18:36:38 +0100

    remove unused imports

--- app/pages/LoginPage.js
+++ app/pages/LoginPage.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, Text, View, TouchableHighlight, AlertIOS, AsyncStorage } from 'react-native';
 
-import HomePage from './HomePage';
-import TestPage from './TestPage';
-import LoginForm from '../components/LoginForm';
-import BuyItButton from '../components/BuyItButton';
-import ProfileButton from '../components/ProfileButton';
 import LoginContainer from '../containers/LoginContainer'
 
 import styles from '../styles'
